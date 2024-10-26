3e586c66cf6f987eccf9c0261b73a38dee8f90b4
Ospina115 <158192117+Ospina115@users.noreply.github.com>
2024-10-26 22:50:46 +0500

    :airplane: Diferentes idiomas implementados
    aqui ya existe la posibilidad de cambiar entre español e ingles con total libertad, ademas, cambié el idioma principal de la pagina por el español porque eso fue lo que se me fue solicitado

--- src/script.js
+++ src/script.js
@@ -1,3 +1,7 @@
+// *****************************
+// **** LENIS SMOOTH SCROLL ****
+// *****************************
+
 import Lenis from 'lenis'
 // Initialize Lenis
 const lenis = new Lenis();
@@ -14,3 +18,26 @@ function raf(time) {
 }
 
 requestAnimationFrame(raf);
+
+// ************************
+// **** CAMBIAR IDIOMA ****
+// ************************
+
+const button = document.getElementById('boton-cambiar-idioma');
+let isSpanish = true; // Estado inicial, español
+
+button.addEventListener('click', () => {
+    // Cambiar el estado del idioma
+    isSpanish = !isSpanish;
+
+    const images = document.querySelectorAll('[data-lang-image]');
+    images.forEach(image => {
+        const newImage = isSpanish ? image.getAttribute('data-lang-es') : image.getAttribute('data-lang-en');
+        image.src = newImage; // Cambia la fuente de la imagen
+    });
+
+    // Alternar el contenido
+    document.querySelectorAll('[data-lang-en], [data-lang-es]').forEach(element => {
+        element.textContent = isSpanish ? element.getAttribute('data-lang-es') : element.getAttribute('data-lang-en');
+    });
+});
